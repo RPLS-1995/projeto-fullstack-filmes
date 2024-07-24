770239d28d07986a715278d8f8b21728cb431de3
Renan Souza <renanec013@gmail.cm>
2024-07-24 00:47:41 -0300

    Adicionar variável de ambiente e atualizar URL do endpoint

--- backend/main.py
+++ backend/main.py
@@ -1,17 +1,20 @@
 # main.py
 from fastapi import FastAPI, HTTPException
 import requests
+import os
 
 app = FastAPI()
 
 
 @app.get('/filmes')
 async def buscar_filmes():
-    # Endpoint para buscar filmes na API The One. 
-    api_url = 'https://the-one-api.dev/v2/movie&apikey=your_api_key'  # Substitua com sua API key
+    # Endpoint para buscar filmes na API The One.
+    api_url = 'https://the-one-api.dev/v2/movie'
+
+    API_KEY = os.getenv('API_KEY')
 
     # Realizando a requisição GET
-    response = requests.get(api_url)
+    response = requests.get(api_url, headers={'Authorization': 'Bearer ' + API_KEY})
 
     # Verificando se a requisição foi bem sucedida (código 200)
     if response.status_code == 200:
